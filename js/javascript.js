dce46d8773ed8b5eafd917a6196b490cfde2f389
Kyle <kylekung@hotmail.com>
2016-02-29 16:59:51 -0500

    added more quotes + changed max width

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,10 @@
-var quotes = ["Always have faith. Always have hope.","The key is to make it.","Another one.","Key to more success is clean heart and clean face."];
+var quotes = [
+	"Always have faith. Always have hope.",
+	"The key is to make it.","Another one.",
+	"Key to more success is clean heart and clean face.",
+	"You smart! You loyal! You’re a genius!",
+	"We go hard. In everything we do we're going to accomplish our victory and our goal. If it takes a day, a year, or 20 years, we're going to win. I haven't taken a loss because everything I've done has been a working process to win. From being a kid on them turntables to becoming where I am is not a loss. It's a blessing."
+	];
 var currentQuote = 0;
 $(function(){
 	$("#inspirational-quote").text(quotes[currentQuote]);
