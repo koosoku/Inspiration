82f8dffb63e5798c72f5a4043a1810f61d2c7431
Kyle <kylekung@hotmail.com>
2016-02-29 20:28:49 -0500

    Added sounds to match the quotes.

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,14 @@
 var quotes = [
-	"Always have faith. Always have hope.",
-	"The key is to make it.","Another one.",
-	"Key to more success is clean heart and clean face.",
-	"You smart! You loyal! You’re a genius!",
-	"We go hard. In everything we do we're going to accomplish our victory and our goal. If it takes a day, a year, or 20 years, we're going to win. I haven't taken a loss because everything I've done has been a working process to win. From being a kid on them turntables to becoming where I am is not a loss. It's a blessing."
+	{quote:"Win win win no matter what", sound:"sounds/winWinWin.mp3"},
+	{quote:"another one", sound:"sounds/anotherOne.mp3"},
+	{quote:"I changed a lot, you can too", sound:"sounds/changedAlot.mp3"},
+	{quote:"Them doors that were closed, I rip the doors off and took the hinges off. And when I took the hinges off, I put the hinges in the fuccboi's hands. Ya, in the fuccboi's hands, you know I took the whole door off and put the hinges in the fuccboi's hands. You can put the hinges on their hands too.", sound:"sounds/doorsHinges.mp3"},
+	{quote:"Put this money in your savings account. Buy your mama a house, buy your whole family houses.", sound:"sounds/mamaHouse.mp3"},
+	{quote:"You smart, You very smart. We the best. You a genius. I appreciate you. You loyal.", sound:"sounds/youSmart.mp3"}
 	];
 var currentQuote = 0;
 $(function(){
-	$("#inspirational-quote").text(quotes[currentQuote]);
+	$("#inspirational-quote").text(quotes[currentQuote].quote);
 });
 
 window.addEventListener("beforeunload", function (e) {
@@ -26,9 +27,11 @@ $(function() {
 			newQuote = Math.floor(Math.random()*quotes.length);
 		}while(currentQuote == newQuote)
 		currentQuote = newQuote;
+		var audio = new Audio(quotes[currentQuote].sound);
+		audio.play();
 
 		$("#inspirational-quote").fadeOut(function(){
-			$(this).text(quotes[currentQuote]);
+			$(this).text(quotes[currentQuote].quote);
 			$(this).fadeIn();
 		})
 
